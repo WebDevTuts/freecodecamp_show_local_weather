ab13a93127fd66f2a2d1e210e7228bb223f8499b
edwinchen85 <edwinchen85@gmail.com>
2017-05-30 01:55:11 +0800

    Define jsonFlickrApi() func

--- script.js
+++ script.js
@@ -67,3 +67,14 @@ function loadBackground(lat, lon, weatherTag) {
 
   document.getElementsByTagName('head')[0].appendChild(script_element);
 }
+
+function jsonFlickrApi(data) {
+  if (data.photos.pages > 0) {
+    var photo = data.photos.photo[Math.floor(Math.random() * parseInt(data.photos.photo.length))];
+    document.querySelector("body").style.backgroundImage = "url('" + photo.url_l + "')";
+    document.querySelector("#image-source").setAttribute("href", "http://www.flickr.com/photos/" + photo.owner + "/" + photo.id);
+  } else {
+    document.querySelector("body").style.backgroundImage = "url('https://fourtonfish.com/tutorials/weather-web-app/images/default.jpg')";
+    document.querySelector("#image-source").setAttribute("href", "https://www.flickr.com/photos/superfamous/310185523/sizes/o/");
+  }
+}
