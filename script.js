26520cfe4a551f2b0c6171c540d92243749cffaf
edwinchen85 <edwinchen85@gmail.com>
2017-05-30 01:39:42 +0800

    Define switchUnits() func to switch temperature units of Fahrenheitor Celsius

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ function roundTemperature(temp) {
   temp = temp.toFixed(1);
   return temp;
 }
+
+function switchUnits() {
+  if (weatherData.units == "°C") {
+    weatherData.temperatureValue = roundTemperature(weatherData.temperatureValue * 9/5 + 32);
+    weatherData.units = "°F";
+  } else {
+    weatherData.temperatureValue = roundTemperature((weatherData.temperatureValue - 32) * 5/9);
+    weatherData.units = "°C";
+  }
+
+  weatherData.temperature.innerHTML = weatherData.temperatureValue + weatherData.units;
+}
